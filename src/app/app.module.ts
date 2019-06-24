ffff5457ca0739ad796779a048ad6e343d08fbca
praiwan-composer <sonice_team@hotmail.com>
2019-06-24 14:14:16 +0700

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { TextComponent } from './text/text.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, TextComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
